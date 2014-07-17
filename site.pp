165981d7ff0af265c7be40fe87b130d1c5f077da
dmdale1 <dmdale@indiana.edu>
2014-07-17 14:30:10 -0400

    modified:   site.pp

--- site.pp
+++ site.pp
@@ -1,9 +1,68 @@
+define accounts::virtual ($uid,$realname,$pass, $title) {
+
+	user {$title:
+		ensure => 'present',
+		uid => $uid,
+		gid => $title,
+		shell => '/bin/bash',
+		#home => "/home/$title",
+		comment => $realname,
+		password => $pass,
+		managehome => true,
+		require => Group[$title],
+	}
+	
+	group {$titel:
+		gid => $uid,
+	}
+
+	file {"/home/name":
+		ensure => directory,
+		owner => $title,
+		group => $title,
+		mode => 0750,
+		require => [ User[$title], Group[$title] ],
+	}
+}
+
+class accounts {
+
+	@accounts::virtual { 'johndoe':
+		title => 'johndoe',
+		uid => 1001,
+		realname => 'John Doe',
+		pass => '<password hash here>',
+	}
+}
+
+node default {
+}
+
+node 'puppet.poc.apparatus.net {
+	include accounts
+	realize (Accounts::Virtual['johndoe'])
+}
+
+
+
+
+
+
+
+
+
+
+
 file { "testfile":
-	ensure => present,
+	ensure => absent,
         path => '/etc/testfile',
 	content => "I'm a test file",
 }
 user { 'dylan':
-	ensure => present,
+	ensure => absent,
 	home => '/home/dylan',
+}
+
+file { '/home/dylan':
+	ensure => absent,
 }
